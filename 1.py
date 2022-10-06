de45fc2b9ccc8317131532fc9b2d556ef457852b
knursana <115156804+knursana@users.noreply.github.com>
2022-10-06 18:44:09 +0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+import sys, cv2 as cv # Импортируем системный модуль
+# Импортируем модуль OpenCV (cv2) под именем cv
+img = cv.imread("astana.jpg", 1) # Загружаем изображение
+cv.imshow("original", img) # Отрисовываем изображение
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# Конвертируем цветное изображение в монохромное
+gray = cv.GaussianBlur(gray, (7, 7), 1.5)
+# Добавляем размытие
+edges = cv.Canny(gray, 0, 50) # Запускаем детектор ребер
+cv.imshow("edges", edges) # Отображаем результат
+cv.waitKey() # Ожидаем нажатия любой клавиши для завершения работы
